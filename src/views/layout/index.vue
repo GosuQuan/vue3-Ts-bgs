3439d8446ab1a6c33822b0514c83fe6f03d1cf75
李权 <11483845+kingsleyesovo@user.noreply.gitee.com>
2024-03-08 14:31:45 +0800

    feat: style for layout

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="layout_container">1
+  <div class="layout_container">
+
     <div class="layout_slider">123</div>
     <div class="layout_tabbar">213</div>
     <div class="layout_main">321</div>
@@ -8,15 +9,30 @@
 
 <script setup lang="ts"></script>
 
-<style scoped>
+<style scoped lang="scss">
 .layout_container {
   width: 100%;
-  height: 100%;
-  background-color: aqua;
+  height: 100vh;
+  background-color: red;
+
 
   .layout_slider {
     width: $base-menu-width;
-
+    height: 100vh;
+    background: $base-menu-background;
   }
+
+  .layout_tabbar {
+    position: fixed;
+    width: calc(100% - $base-menu-width);
+    background-color: aqua;
+    top: 0px;
+    left: $base-menu-width;
+    height: $base-tabbar-height;
+  }
+  .layout_main{
+    
+  }
+
 }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   return {
+    //css config
     css: {
       preprocessorOptions: {
         scss: {
