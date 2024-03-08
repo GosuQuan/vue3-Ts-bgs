f35e7ae9e955e787edc616867c8742f244fc848c
李权 <11483845+kingsleyesovo@user.noreply.gitee.com>
2024-03-08 13:15:23 +0800

    feat: add Time module

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,17 +1,16 @@
 export const getTime = () => {
-    let msg = "";
-    let hours = new Date().getHours();
-    if (hours > 5 && hours < 11) {
-        msg = "早上";
-        return msg;
-    }
-    if (hours > 10 && hours < 13) {
-        msg = "中午";
-        return msg;
-    }
-    if (hour > 0 && hours < 6 || hours > 19 && hours < 24) {
-        msg = "晚上";
-        return msg;
-    }
-
-}
+  let msg = "";
+  let hours = new Date().getHours();
+  if (hours > 5 && hours < 11) {
+    msg = "早上";
+    return msg;
+  }
+  if (hours > 10 && hours < 13) {
+    msg = "中午";
+    return msg;
+  }
+  if ((hour > 0 && hours < 6) || (hours > 19 && hours < 24)) {
+    msg = "晚上";
+    return msg;
+  }
+};
